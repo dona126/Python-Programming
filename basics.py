c0a1fb9a486b2cb43399c5b81c751510ea3803a1
dona_maria_sunny <47379075+dona126@users.noreply.github.com>
2020-06-18 09:21:30 +0530

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,47 @@
+#basics
+
+#here no data type, no semicolon, no braces
+#...........but colon, intendation, logical operators written in words like "and" are prsnt :)
+
+#LIMFSC for list,tuple,string
+
+#built in ds...list,tuple,dict,set
+#user defined ds.....linkedlist,stack,queue,tree,graph
+
+
+#IN PYTHON print("no. is ",num)
+#IN C printf("no. is %s",num);
+#IN JAVA Sysyem.out,print("no. is"+num);
+
+
+
+
+name="helena"
+age=3
+print("name is ",name,"and age is ",age)
+print("")
+
+a=1+2
+print(a)
+print("")
+
+
+word="hello "+"world"
+print(word)
+print("")
+
+ab=float(1)+2#type conversion
+print(ab)
+print("")
+
+print(9/2)# in python 3, these integers give float as ans unlike pythpon 2.
+print("")
+
+#functions in python
+
+def sum(x,y):
+ sum=x+y
+ return sum#fn ended
+
+
+print("Ans is",sum(3,5))
