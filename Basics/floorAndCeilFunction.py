5663b75c2a6d36517965781346fd715c0d3821c8
dona_maria_sunny <47379075+dona126@users.noreply.github.com>
2020-08-05 10:58:24 +0530

    Add files via upload

---
+++ Basics/floorAndCeilFunction.py
@@ -0,0 +1,13 @@
+#floor fn - rounds down
+#ceil fn - rounds up
+
+import math
+num=12.3
+num2=12.7
+print("math.floor(num)", math.floor(num))
+print("math.floor(num2)", math.floor(num2))
+print("math.ceil(num)", math.ceil(num))
+print("math.ceil(num2)", math.ceil(num2))
+
+#floor fn - rounds down
+#ceil fn - rounds up
