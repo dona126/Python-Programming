79724d4810687124d2ad58778838d6093e513f2a
dona_maria_sunny <47379075+dona126@users.noreply.github.com>
2020-08-05 11:12:21 +0530

    Update floorAndCeilFunction.py

--- Basics/floorAndCeilFunction.py
+++ Basics/floorAndCeilFunction.py
@@ -9,3 +9,7 @@ print("math.floor(num2): ", math.floor(num2))
 print("math.ceil(num): ", math.ceil(num))
 print("math.ceil(num2): ", math.ceil(num2))
 
+#round(x,y) - function to round numbers
+#x - number to be rounded
+#y - number of digits to be present after decimal point
+print(round(34.58,1))
