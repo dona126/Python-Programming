3536ca9c81f62af8f932b387a3e314df439de511
dona_maria_sunny <47379075+dona126@users.noreply.github.com>
2020-08-05 11:03:05 +0530

    Update floorAndCeilFunction.py

--- Basics/floorAndCeilFunction.py
+++ Basics/floorAndCeilFunction.py
@@ -4,8 +4,8 @@
 import math
 num=12.3
 num2=12.7
-print("math.floor(num)", math.floor(num))
-print("math.floor(num2)", math.floor(num2))
-print("math.ceil(num)", math.ceil(num))
-print("math.ceil(num2)", math.ceil(num2))
+print("math.floor(num): ", math.floor(num))
+print("math.floor(num2): ", math.floor(num2))
+print("math.ceil(num): ", math.ceil(num))
+print("math.ceil(num2): ", math.ceil(num2))
 
