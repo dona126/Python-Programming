b51f82604243ffb0331553086a968df68328cfc4
dona_maria_sunny <47379075+dona126@users.noreply.github.com>
2020-08-06 17:00:06 +0530

    Update array.py

--- userDefinedDataStructures/array.py
+++ userDefinedDataStructures/array.py
@@ -1,1 +1,28 @@
+#Array is like list but store data of one data type.
+
+import array as arr
+a = arr.array('i', [1, 2, 3])#i shows data type Integer
+
+#Operations performed in an array
+while(True):
+    print('1. Print elements of array')
+    print('2. Add element to array')
+    print('3. Delete element from array')
+    print('4. Exit')
+    choic=input("Enter your choice: ")
+    choice=int(choic)
+    if(choice==1):
+        print('Array elements are:')
+        for number in a:
+            print(number)
+    elif(choice==2):
+        val = int(input('Enter integer number:'))
+        a.append(val);
+    elif(choice==3):
+        value = a.pop()
+        print('Value deleted:', value)
+    elif(choice==4):
+        break
+    else:
+        print("Enter valid choice.")
 
