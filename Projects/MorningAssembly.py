e5362baaa2ef73195a13399b64aa5ffe7dd0cc1c
dona_maria_sunny <47379075+dona126@users.noreply.github.com>
2020-08-06 20:27:35 +0530

    Update MorningAssembly.py

--- Projects/MorningAssembly.py
+++ Projects/MorningAssembly.py
@@ -1,25 +1,27 @@
-#A classroom has several students, half of whom are boys and half of whom are girls.
-#You need to arrange all of them in a line for the morning assembly such that the following conditions are satisfied:
-#  1.The students must be in order of non-decreasing height.
-#  2.Two boys or two girls must not be adjacent to each other.
-#You have been given the heights of the boys in the array and the heights of the girls in the array . Find out whether you can arrange them in an order which satisfies
-#the given conditions. Print "YES" if it is possible, or "NO" if it is not.
+
+
+#There are equal number of boys and girls in a classroom.
+#You need to arrange all of them in a line for the morning assembly such that the students must be in order of
+#non-decreasing height and also no two boys or two girls must not be adjacent to each other.
+#You have been given the heights of the boys and the heights of the girls. Find out whether
+#you can arrange them in an order which satisfies the given conditions.
+#Print "YES" if it is possible, or "NO" if it is not.
 
 test=int(input("Enter number of test cases:"))
 i=1
 l1=[]
 l2=[]
 while((i<test) or (i==test)):
-    n=int(input("Enter number of boys/girls"))
+    n=int(input("Enter number of boys/girls:"))
     j=1
     while((j<n) or (j==n)):
-        h=int(input())
+        h=int(input("Enter height of  boy:"))
         l1.append(h)
         l1.sort()
         j=j+1
     j=1
     while((j<n) or (j==n)):
-        h=int(input("Enter height of %d boys/girls:", n))
+        h=int(input("Enter height of  girl:"))
         l2.append(h)
         l2.sort()
         j=j+1
@@ -43,7 +45,7 @@ while((i<test) or (i==test)):
             person=l3[n]
             l4.append(person)
             n=n+1
-    print(l4)
+    print("Possible arrangement: ",l4)
 
     if(len(l4)==len(l3)):
         print("YES")
