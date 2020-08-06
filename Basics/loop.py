f7c7192a2d7086bba20aa88115478ab4e5ce0b4c
dona_maria_sunny <47379075+dona126@users.noreply.github.com>
2020-08-06 17:03:50 +0530

    Add files via upload

---
+++ Basics/loop.py
@@ -0,0 +1,15 @@
+#for loop(increment done automatically)
+
+myList=[1,2,3,"meera"]
+for x in myList:
+ print(x)
+
+
+#while loop(increment not done automatically)
+i=0
+while(i<4):
+ print(myList[i])
+ i=i+1
+
+#break : exit from the loop
+#continue : flow goes to beginning of loop
