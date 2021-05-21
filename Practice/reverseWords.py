f7ba8301799263f9096cb4047e37eeb03b2aa7bb
Dona Maria Sunny <47379075+dona126@users.noreply.github.com>
2021-05-21 17:39:20 +0530

    Update reverseWords.py

--- Practice/reverseWords.py
+++ Practice/reverseWords.py
@@ -1,14 +1,9 @@
 # Given a String S, reverse the string without reversing its individual words. Words are separated by dots.
-#
-# Example 1:
-#
+
 # Input:
-# S = i.like.this.program.very.much
-# Output: much.very.program.this.like.i
-# Explanation: After reversing the whole
-# string(not individual words), the input
-# string becomes
-# much.very.program.this.like.i
+# S = i.wake.up.early
+# Output: early.up.wake.i
+
 S=input("Enter String to be reversed:")
 list1=S.split(".")
 length_list1=len(list1)-1
