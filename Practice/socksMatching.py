c29b9ca4875963ca1fcfec471528de55b5ffea13
Dona Maria Sunny <47379075+dona126@users.noreply.github.com>
2021-06-19 20:30:17 +0530

    Update socksMatching.py

--- Practice/socksMatching.py
+++ Practice/socksMatching.py
@@ -1,6 +1,7 @@
 # Given an array of integers representing the color of each sock, find how many pairs of socks with matching colors are there.
 # Example
-#arr = [1, 2, 1, 2, 1, 3, 2, 10, 10, 80, 80]
+#arr = [10, 10, 8, 8, 1, 1, 1, 1, 1, 5]
+#Output: 4
 
 
 import math
